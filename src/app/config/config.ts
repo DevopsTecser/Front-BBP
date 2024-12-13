365896a9f9c1e058ad5bdbb90a64c9c5a6e71a27
Desarrolladorfist <desarrollador1@tecsersas.com>
2024-12-13 10:38:43 -0500

    Routes

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    apiHost: 'http://localhost:5500', // Host centralizado
+    apiHost: 'http://192.168.2.4:5500', // Host centralizado
     apiEndpoint: '/v1/endpoint',
     anotherConfigValue: 'valorEjemplo',
   };
