ecfd73c2b456a35f2f9848d1ad4ea29060c1a1ff
Desarrolladorfist <desarrollador1@tecsersas.com>
2024-12-13 18:05:31 -0500

    Change general style part1

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    apiHost: 'http://192.168.2.4:5500', // Host centralizado
+    apiHost: 'http://192.168.2.19:5500', // Host centralizado
     apiEndpoint: '/v1/endpoint',
     anotherConfigValue: 'valorEjemplo',
   };
